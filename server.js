8a1e3afbc8e99202c4b94254610e16455886c383
barbarahoehne <barbara.hoehne@bondy.com.br>
2026-06-09 16:10:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 const CU = 'https://api.clickup.com/api/v2';
-
-function getToken(req) {
-  return req.headers['x-token'] || req.headers['authorization'] || '';
-}
+const TOKEN = 'pk_84198557_25C10VCV5M5HY7WEXFVBPJ83A6X11EKA';
 
 app.get('/tasks/:listId', async (req, res) => {
   try {
-    const token = getToken(req);
     const r = await fetch(`${CU}/list/${req.params.listId}/task?include_closed=true`, {
-      headers: { Authorization: token }
+      headers: { Authorization: TOKEN }
     });
     const d = await r.json();
     res.json(d);
@@ -25,10 +21,9 @@ app.get('/tasks/:listId', async (req, res) => {
 
 app.post('/tasks/:listId', async (req, res) => {
   try {
-    const token = getToken(req);
     const r = await fetch(`${CU}/list/${req.params.listId}/task`, {
       method: 'POST',
-      headers: { Authorization: token, 'Content-Type': 'application/json' },
+      headers: { Authorization: TOKEN, 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body)
     });
     const d = await r.json();
@@ -38,8 +33,7 @@ app.post('/tasks/:listId', async (req, res) => {
 
 app.get('/user', async (req, res) => {
   try {
-    const token = getToken(req);
-    const r = await fetch(`${CU}/user`, { headers: { Authorization: token } });
+    const r = await fetch(`${CU}/user`, { headers: { Authorization: TOKEN } });
     const d = await r.json();
     res.json(d);
   } catch(e) { res.status(500).json({ err: e.message }); }
